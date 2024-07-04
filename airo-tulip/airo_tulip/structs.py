2f501678823b7c5be2a5893e458816b62577ddae
Mathieu De Coster <mathieu.decoster@ugent.be>
2024-07-04 09:32:13 +0200

    Use field(default_factory) for mutable dataclass fields

--- airo-tulip/airo_tulip/structs.py
+++ airo-tulip/airo_tulip/structs.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+
 
 @dataclass
 class Point2D:
@@ -7,6 +8,7 @@ class Point2D:
     """
     x: float = 0.0
     y: float = 0.0
+
 
 @dataclass
 class Attitude2D:
@@ -17,6 +19,7 @@ class Attitude2D:
     x: float = 0.0
     y: float = 0.0
     a: float = 0.0
+
 
 @dataclass
 class PlatformLimits:
@@ -30,6 +33,7 @@ class PlatformLimits:
     max_dec_linear: float = 0.5
     max_dec_angular: float = 0.8
 
+
 @dataclass
 class WheelConfig:
     """
@@ -40,25 +44,26 @@ class WheelConfig:
     y: float
     a: float
 
+
 @dataclass
 class WheelData:
     enable: bool
     error: bool
     error_timestamp: bool
 
+
 @dataclass
 class WheelParamVelocity:
     """
     Class for storing the details and geometry of each drive as a part of the complete mobile platform.
     """
-    pivot_position:             Point2D     = Point2D()     # pivot location relative to vehicle centre
-    pivot_offset:               float       = 0.0           # pivot offset relative to vehicle direction of travel
-    relative_position_l:        Point2D     = Point2D()     # location of left wheel relative to pivot
-    relative_position_r:        Point2D     = Point2D()     # location of right wheel relative to pivot
-    linear_to_angular_velocity: float       = 0.0           # scaling m/s to rad/s
-    angular_to_linear_velocity: float       = 0.0           # scaling rad/s to m/s
-    max_linear_velocity:        float       = 0.0           # maximum velocity of wheel
-    max_pivot_velocity:         float       = 0.0           # maximum pivot error of smart wheel used for error correction
-    pivot_kp:                   float       = 0.0           # proportional gain for pivot position controller
-    wheel_diameter:             float       = 0.0           # wheel diameter
-
+    pivot_position: Point2D = field(default_factory=Point2D)  # pivot location relative to vehicle centre
+    pivot_offset: float = 0.0  # pivot offset relative to vehicle direction of travel
+    relative_position_l: Point2D = field(default_factory=Point2D)  # location of left wheel relative to pivot
+    relative_position_r: Point2D = field(default_factory=Point2D)  # location of right wheel relative to pivot
+    linear_to_angular_velocity: float = 0.0  # scaling m/s to rad/s
+    angular_to_linear_velocity: float = 0.0  # scaling rad/s to m/s
+    max_linear_velocity: float = 0.0  # maximum velocity of wheel
+    max_pivot_velocity: float = 0.0  # maximum pivot error of smart wheel used for error correction
+    pivot_kp: float = 0.0  # proportional gain for pivot position controller
+    wheel_diameter: float = 0.0  # wheel diameter
