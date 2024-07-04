459e0f320f3afb61cf6bbe11c6e001ba53d8b1f1
bitscuity <thomas@bitscuit.be>
2024-07-04 13:55:33 +0200

    add logger in robile platform

--- airo-tulip/airo_tulip/robile_platform.py
+++ airo-tulip/airo_tulip/robile_platform.py
@@ -1,5 +1,6 @@
 import pysoem
 from typing import List
+from loguru import logger
 
 from airo_tulip.structs import WheelConfig
 from airo_tulip.platform_driver import PlatformDriver
@@ -34,26 +35,26 @@ class RobilePlatform():
         # Configure slaves
         wkc = self._master.config_init()
         if wkc == 0:
-            print("No EtherCAT slaves were found.")
+            logger.warning("No EtherCAT slaves were found.")
             self._master.close()
             return False
 
         self._master.config_map()
-        print(f"Found {len(self._master.slaves)} slaves")
+        logger.info(f"Found {len(self._master.slaves)} slaves")
         for slave in self._master.slaves:
-            print(slave.id, slave.man, slave.name)
+            logger.info(slave.id, slave.man, slave.name)
 
         # Check if all slaves reached SAFE_OP state
         self._master.read_state()
         requested_state = EC_STATE_SAFE_OP
         found_state = self._master.state_check(requested_state)
         if found_state != requested_state:
-            print("Not all EtherCAT slaves reached a safe operational state.")
+            logger.warning("Not all EtherCAT slaves reached a safe operational state.")
             # TODO: check and report which slave was the culprit.
             return False
         
         # Request OP state for all slaves
-        print("Requesting operational state for all EtherCAT slaves.")
+        logger.info("Requesting operational state for all EtherCAT slaves.")
         self._master.state = EC_STATE_OPERATIONAL
         self._master.send_processdata()
         self._master.receive_processdata()
@@ -63,13 +64,13 @@ class RobilePlatform():
         requested_state = EC_STATE_OPERATIONAL
         found_state = self._master.state_check(requested_state)
         if found_state == requested_state:
-            print("All EtherCAT slaves reached a safe operational state.")
+            logger.info("All EtherCAT slaves reached a safe operational state.")
         else:
-            print("Not all EtherCAT slaves reached a safe operational state.")
+            logger.warning("Not all EtherCAT slaves reached a safe operational state.")
             return False
 
         for slave in self._master.slaves:
-            print(f"name {slave.name} Obits {len(slave.output)} Ibits {len(slave.input)} state {slave.state}")
+            logger.debug(f"name {slave.name} Obits {len(slave.output)} Ibits {len(slave.input)} state {slave.state}")
 
         return True
 
