3aa1d69aa35044e6fb3e8c470a0758ff1f2e8324
Mathieu De Coster <mathieu.decoster@ugent.be>
2024-07-08 11:37:33 +0200

    Add rerun-sdk dependency, increment version to 0.0.2, update README

--- airo-tulip/setup.py
+++ airo-tulip/setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages
 
 setuptools.setup(
     name="airo-tulip",
-    version="0.0.1",
+    version="0.0.2",
     author="Mathieu De Coster",
     author_email="mathieu.decoster@ugent.be",
     description="Python driver for the KELO Robile platform",
-    install_requires=["numpy==1.24.4", "pyzmq==26.0.3", "loguru>=0.7.2", "pysoem>=1.1.6"],
+    install_requires=["numpy==1.24.4", "pyzmq==26.0.3", "loguru>=0.7.2", "pysoem>=1.1.6", "rerun-sdk>=0.16.1"],
     packages=find_packages(),
 )
