5daf09c552fc9f4b4986db684444e394e0632b9b
Mathieu De Coster <mathieu.decoster@ugent.be>
2024-07-09 13:59:25 +0200

    Joystick testing script

---
+++ airo-tulip/scripts/test_joystick.py
@@ -0,0 +1,59 @@
+import math
+import time
+
+import numpy as np
+import pygame
+from airo_tulip.server.kelo_robile import KELORobile
+
+pygame.init()
+
+# Logitech F310
+AXIS_LEFT_HORIZONTAL = 0
+AXIS_LEFT_VERTICAL = 1
+AXIS_RIGHT_HORIZONTAL = 3
+AXIS_RIGHT_VERTICAL = 4
+
+LINEAR_SPEED_SCALING = 0.5  # joystick range: [-1, 1]. This maps it to [-LINEAR_SPEED_SCALING, LINEAR_SPEED_SCALING] m/s.
+ANGULAR_SPEED_SCALING = math.pi / 8  # analogous for angular velocity in rad/s.
+
+if not pygame.joystick.get_init():
+    exit(1)
+
+if not pygame.joystick.get_count() > 0:
+    pygame.joystick.quit()
+    exit(2)
+
+joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+joystick = joysticks[0]
+print("num axes:", joystick.get_numaxes())
+
+mobi = KELORobile("10.10.129.20", 49789)
+
+delta_time = 0
+start = time.time()
+while True:
+    for _ in pygame.event.get():
+        pass
+
+    # Y axis movement.
+    axis_left_horizontal = joystick.get_axis(AXIS_LEFT_HORIZONTAL)
+    # X axis movement.
+    axis_left_vertical = joystick.get_axis(AXIS_LEFT_VERTICAL)
+    # Angular movement.
+    axis_right_horizontal = joystick.get_axis(AXIS_RIGHT_HORIZONTAL)
+
+    vel_x = LINEAR_SPEED_SCALING * -axis_left_vertical * delta_time
+    vel_y = LINEAR_SPEED_SCALING * axis_left_horizontal * delta_time
+    vel_a = ANGULAR_SPEED_SCALING * axis_right_horizontal * delta_time
+
+    vel_xy = np.array([vel_x, vel_y])
+    vel_xy /= np.linalg.norm(vel_xy) + 1e-6
+    vel_x, vel_y = vel_xy
+
+    mobi.set_platform_velocity_target(vel_x, vel_y, vel_a)
+
+    now = time.time()
+    delta_time = now - start
+    start = now
+
+pygame.joystick.quit()
--- airo-tulip/setup.py
+++ airo-tulip/setup.py
@@ -7,6 +7,6 @@ setuptools.setup(
     author="Mathieu De Coster",
     author_email="mathieu.decoster@ugent.be",
     description="Python driver for the KELO Robile platform",
-    install_requires=["numpy==1.24.4", "pyzmq==26.0.3", "loguru>=0.7.2", "pysoem>=1.1.6", "rerun-sdk>=0.16.1"],
+    install_requires=["numpy==1.24.4", "pyzmq==26.0.3", "loguru>=0.7.2", "pysoem>=1.1.6", "rerun-sdk>=0.16.1", "pygame>=2.6.0"],
     packages=find_packages(),
 )
