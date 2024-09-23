03d8364b7d9a066cbbb191540075afe3af1eabbf
Mathieu De Coster <mth.decoster@gmail.com>
2024-09-23 09:49:51 +0200

    Increase velocities and times

--- airo-tulip/scripts/test_kelo_robile_compliant.py
+++ airo-tulip/scripts/test_kelo_robile_compliant.py
@@ -9,16 +9,16 @@ def move_direction(mobi, x, y, a):
     time.sleep(2.0)
 
     mobi.set_driver_type(PlatformDriverType.COMPLIANT_WEAK)
-    mobi.set_platform_velocity_target(x, y, a, timeout=2.0)
+    mobi.set_platform_velocity_target(x, y, a, timeout=5.0)
 
 
 def test():
     mobi = KELORobile("localhost", 49789)
 
-    move_direction(mobi, 0.2, 0.0, 0.0)
-    move_direction(mobi, 0.0, 0.2, 0.0)
-    move_direction(mobi, -0.2, 0.0, 0.0)
-    move_direction(mobi, 0.0, -0.2, 0.0)
+    move_direction(mobi, 0.5, 0.0, 0.0)
+    move_direction(mobi, 0.0, 0.5, 0.0)
+    move_direction(mobi, -0.5, 0.0, 0.0)
+    move_direction(mobi, 0.0, -0.5, 0.0)
 
     mobi.stop_server()
     time.sleep(0.5)
