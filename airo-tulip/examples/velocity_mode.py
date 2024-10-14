a270027d32f7cc8cb8d12386db797eb4114f6245
Mathieu De Coster <mth.decoster@gmail.com>
2024-10-14 09:14:53 +0200

    Remove instantaneous argument

--- airo-tulip/examples/velocity_mode.py
+++ airo-tulip/examples/velocity_mode.py
@@ -10,19 +10,19 @@ def test():
     mobi.set_platform_velocity_target(0.2, 0.0, 0.0)
     time.sleep(3)  # movement should timeout
 
-    mobi.set_platform_velocity_target(0.0, 0.0, math.pi / 8, timeout=2.0, instantaneous=True)
+    mobi.set_platform_velocity_target(0.0, 0.0, math.pi / 8, timeout=2.0)
     time.sleep(2)
 
-    mobi.set_platform_velocity_target(0.2, 0.0, 0.0, instantaneous=True)
+    mobi.set_platform_velocity_target(0.2, 0.0, 0.0)
     time.sleep(1)
 
-    mobi.set_platform_velocity_target(0.0, 0.2, 0.0, instantaneous=True)
+    mobi.set_platform_velocity_target(0.0, 0.2, 0.0)
     time.sleep(1)
 
-    mobi.set_platform_velocity_target(-0.2, 0.0, 0.0, instantaneous=True)
+    mobi.set_platform_velocity_target(-0.2, 0.0, 0.0)
     time.sleep(1)
 
-    mobi.set_platform_velocity_target(0.0, -0.2, 0.0, instantaneous=True)
+    mobi.set_platform_velocity_target(0.0, -0.2, 0.0)
     time.sleep(1)
 
     mobi.set_platform_velocity_target(0.0, 0.0, -math.pi / 8, timeout=2.0)
@@ -31,7 +31,7 @@ def test():
     mobi.set_platform_velocity_target(-0.2, 0.0, 0.0, timeout=3.0)
     time.sleep(3)  # movement should timeout
 
-    mobi.set_platform_velocity_target(0.0, 0.0, 0.0, instantaneous=True)
+    mobi.set_platform_velocity_target(0.0, 0.0, 0.0)
     time.sleep(0.5)
 
     mobi.stop_server()
